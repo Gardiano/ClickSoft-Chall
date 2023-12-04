85b2326cd7452ffe5f2e3aaa725d93334be821bb
Gardiano <joaopaulong@hotmail.com>
2023-12-04 12:49:23 -0300

    context added for state management between components

--- src/components/context/UserContext.tsx
+++ src/components/context/UserContext.tsx
@@ -8,15 +8,36 @@ type UserDataContextProviderProps = {
 
 type AuthContextTypes = {
   data: users | undefined;
-  setData: React.Dispatch< React.SetStateAction <users | undefined> >;
+  openMenu: boolean
+  searchedUsers: users | undefined
+  setSearchedUsers: React.Dispatch<React.SetStateAction<users | undefined>>;
+  openRecentSearchedUsers: () => void
+  setData: React.Dispatch<React.SetStateAction<users | undefined>>;
+  setOpenMenu: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const UserDataContext = createContext({} as AuthContextTypes);
 
+// Gerenciando states entre components.
 export function UserDataProvider(props: UserDataContextProviderProps) {
   const [data, setData] = useState<users>();
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
+  const [searchedUsers, setSearchedUsers] = useState<users>();
+
+  const openRecentSearchedUsers = () => {
+    setOpenMenu(prevState => !prevState);
+  }
+
   return (
-    <UserDataContext.Provider value={{ data, setData }}  >
+    <UserDataContext.Provider value={{
+      data,
+      setData,
+      searchedUsers,
+      setSearchedUsers,
+      openMenu,
+      setOpenMenu,
+      openRecentSearchedUsers
+    }}>
       {props.children}
     </UserDataContext.Provider>
   );
