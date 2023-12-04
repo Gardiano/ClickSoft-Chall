78d7645088d7932dcfd4c74e3f1f078c65eafc5c
Gardiano <joaopaulong@hotmail.com>
2023-12-04 15:35:32 -0300

    toast container refactoring

--- src/helpers/toastContainer.tsx
+++ src/helpers/toastContainer.tsx
@@ -5,18 +5,18 @@ import 'react-toastify/dist/ReactToastify.css';
 export const Toast = () => {
   return (
     <>
-    <ToastContainer
-      position="bottom-right"
-      autoClose={2500}
-      limit={1}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="colored"
+      <ToastContainer
+        position="bottom-right"
+        autoClose={2500}
+        limit={1}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
       />
     </>
   )
